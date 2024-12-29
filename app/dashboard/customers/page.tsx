8a970334551594f4102a602140634a134e165ce6
porporpor020202 <porporpor020202@gmail.com>
2024-12-29 09:21:56 +0900

    initial

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,25 +1,25 @@
-// import { fetchFilteredCustomers } from '@/app/lib/data';
-// import CustomersTable from '@/app/ui/customers/table';
-// import { Metadata } from 'next';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
 
-// export const metadata: Metadata = {
-//   title: 'Customers',
-// };
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 
-// export default async function Page(props: {
-//   searchParams?: Promise<{
-//     query?: string;
-//     page?: string;
-//   }>;
-// }) {
-//   const searchParams = await props.searchParams;
-//   const query = searchParams?.query || '';
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
 
-//   const customers = await fetchFilteredCustomers(query);
+  const customers = await fetchFilteredCustomers(query);
 
-//   return (
-//     <main>
-//       <CustomersTable customers={customers} />
-//     </main>
-//   );
-// }
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
+}
